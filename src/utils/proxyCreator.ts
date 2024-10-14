1c03abfe91128c9fd952b488459166f6ff6a0b8a
vishnubansaltarento <vishnu.bansal@tarento.com>
2024-10-14 12:44:15 +0530

    update for vapt issue

--- src/utils/proxyCreator.ts
+++ src/utils/proxyCreator.ts
@@ -67,6 +67,25 @@ proxy.on('proxyReq', (proxyReq: any, req: any, _res: any, _options: any) => {
 proxy.on('proxyRes', (proxyRes: any, req: any, _res: any, ) => {
   // res.removeHeader('access-control-allow-origin')
   delete proxyRes.headers['access-control-allow-origin']
+  let connectCookies = proxyRes.headers["set-cookie"]
+  let connectId = ''
+  if(connectCookies && connectCookies.length) {
+    connectCookies[0].split(";").find( (c:any) => {
+      if (c && c.indexOf("connect.sid") > -1) {
+        let splitVal = c.split("=")
+        if(splitVal && splitVal.length > 1) {
+          connectId = c.split("=")[1]
+        }
+        
+      }
+    })
+  }
+  proxyRes.cookie('connect.sid', connectId, {
+    httpOnly: true,
+    maxAge: CONSTANTS.KEYCLOAK_SESSION_TTL,
+    sameSite: 'Strict',
+    secure: true,
+  })
   // write user session with roles
   // if (req.originalUrl.includes('/user/v2/read')) {
   //   // tslint:disable-next-line: no-any
